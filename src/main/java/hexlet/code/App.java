55af8e54b0cd116b571498f054520432d4a59fac
Vitalii Semenov <semvi@list.ru>
2022-08-15 23:11:01 +0300

    Step 3. Add filepath in option string

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,12 +6,20 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", version = "App 2.0", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
-
     @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
     boolean versionInfoRequested;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
     @Override public void run() { System.out.println("Hello World!"); }
 
     public static void main(String[] args) {
