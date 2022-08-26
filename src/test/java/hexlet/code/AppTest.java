002b13929a513dddf70040cb58a1cb1d1f7f713b
Vitalii Semenov <semvi@list.ru>
2022-08-26 19:59:18 +0300

    move differ tests to app tests

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -2,9 +2,9 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-class DifferTest {
+class AppTest {
 
     @Test
     void generateJsonTest() {
