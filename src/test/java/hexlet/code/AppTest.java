6f94f6278c0b6d047287554ced5365d5ee904561
Vitalii Semenov <semvi@list.ru>
2022-08-30 11:00:12 +0300

    add some tests and update grandle for test coverage

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AppTest {
 
     @Test
-    void generateJsonTest() {
+    void generateJsonToJsonTest() {
         String path1 = "src/test/resources/file1.json";
         String path2 = "src/test/resources/file2.json";
         String result;
@@ -25,8 +25,9 @@ class AppTest {
                 + "+ verbose\":true}";
         assertEquals(actual, result);
     }
+
     @Test
-    void generateYmlTest() {
+    void generateYmlToPlainTest() {
         String path1 = "src/test/resources/file1.yml";
         String path2 = "src/test/resources/file2.yml";
         String result;
@@ -48,6 +49,69 @@ class AppTest {
                 Property 'setting2' was updated. From 200 to 300
                 Property 'setting3' was updated. From true to none
                 """;
+        assertEquals(actual, result);
+    }
+
+    @Test
+    void generateJsonToStylishTest() {
+        String path1 = "src/test/resources/file1.json";
+        String path2 = "src/test/resources/file2.json";
+        String result;
+
+        try {
+            result = Differ.generate(path1, path2, "stylish");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String actual = """
+                {
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                }""";
+        assertEquals(actual, result);
+    }
+
+    @Test
+    void generateJsonToRecursionStylishTest() {
+        String path1 = "src/test/resources/file3.json";
+        String path2 = "src/test/resources/file4.json";
+        String result;
+
+        try {
+            result = Differ.generate(path1, path2, "stylish");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String actual = """
+                {
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
+                }""";
         assertEquals(actual, result);
     }
 }
