4b4ffef148a4bc0cc5200dd6c62ad9843ccc3039
Vitalii Semenov <semvi@list.ru>
2022-09-12 23:23:31 +0300

    simpled json formatter and update tests

--- src/main/java/hexlet/code/formatters/Json.java
+++ src/main/java/hexlet/code/formatters/Json.java
@@ -1,30 +1,15 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import hexlet.code.ComparedEntry;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Json {
     public static String format(Map<String, ComparedEntry> diffBuilder) throws Exception {
-        Map<String, Object> outMap = new LinkedHashMap<>();
-
-        for (Map.Entry<String, ComparedEntry> each : diffBuilder.entrySet()) {
-            switch (each.getValue().getAction()) {
-                case ADDED -> outMap.put("+ " + each.getKey(), each.getValue().getValue());
-                case REMOVED -> outMap.put("- " + each.getKey(), each.getValue().getLastValue());
-                case NOTCHANGED -> outMap.put("  " + each.getKey(), each.getValue().getValue());
-                case CHANGED -> {
-                    outMap.put("- " + each.getKey(), each.getValue().getLastValue());
-                    outMap.put("+ " + each.getKey(), each.getValue().getValue());
-                }
-                default -> throw new IllegalStateException("Unexpected action: " + each.getValue().getAction());
-            }
-        }
-
         ObjectMapper mapper = new ObjectMapper();
-        //mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        return mapper.writeValueAsString(outMap) + "\n";
+        mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+        return mapper.writeValueAsString(diffBuilder) + "\n";
     }
 }
